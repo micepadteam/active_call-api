6e7cb7e0e84e9b72d9a1aec2a0ef88271ff0b75c
Kobus Joubert <kobus.joubert@platform45.com>
2025-03-23 12:00:25 +0200

    Initial commit

---
+++ spec/active_call/api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActiveCall::Api do
+  it "has a version number" do
+    expect(ActiveCall::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
