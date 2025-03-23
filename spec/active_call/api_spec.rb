1ebcec137241dae24b23a5117c142af614085eb0
Kobus Joubert <kobus.joubert@platform45.com>
2025-03-23 12:36:28 +0200

    Rubocop config

--- spec/active_call/api_spec.rb
+++ spec/active_call/api_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe ActiveCall::Api do
   it 'has a version number' do
-    expect(ActiveCall::Api::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(ActiveCall::Api::VERSION).not_to be_nil
   end
 end
